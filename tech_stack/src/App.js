fd0e52bb3b58367608243e1745dac632f135b9db
공채원 <chaewonkong@gmail.com>
2018-11-20 03:17:35 +0900

    Fix typo

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -3,14 +3,12 @@ import { View } from "react-native";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducers from "./reducers";
-import Header from "./components/common";
+import { Header } from "./components/common";
 
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
-      <View>
-        <Header headerText="Tech Stack" />
-      </View>
+      <Header headerText="Tech Stack" />
     </Provider>
   );
 };
