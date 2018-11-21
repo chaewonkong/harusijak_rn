6b32e58e7918c69fa8656e887cc5c639f9212a28
공채원 <chaewonkong@gmail.com>
2018-11-21 18:25:05 +0900

    Add FlatList listview

--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
+import { FlatList, Text } from "react-native";
 import { connect } from "react-redux";
+import ListItem from "./ListItem";
 
 class LibraryList extends Component {
+  renderItem(item) {
+    return <ListItem library={item} />;
+  }
   render() {
-    console.log(this.props);
-    return;
+    console.log(this.props.libraries);
+    return (
+      <FlatList
+        data={this.props.libraries}
+        renderItem={({ item }) => this.renderItem(item)}
+        keyExtractor={library => String(library.id)}
+      />
+    );
   }
 }
 
