7ae8b495be99554a00e3dc8568c7a8df4d623357
공채원 <chaewonkong@gmail.com>
2018-12-04 12:54:38 +0900

    Move google key to .gitignore

--- manager/src/App.js
+++ manager/src/App.js
@@ -4,17 +4,11 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducers from "./reducers";
 import firebase from "firebase";
+import authInfo from "../googleAuth";
 
 class App extends Component {
   componentWillMount() {
-    const config = {
-      apiKey: "AIzaSyDcCu1FNx58-W8tAVW7t3wS-yKi9Z-X2Wg",
-      authDomain: "manager-8fd2d.firebaseapp.com",
-      databaseURL: "https://manager-8fd2d.firebaseio.com",
-      projectId: "manager-8fd2d",
-      storageBucket: "manager-8fd2d.appspot.com",
-      messagingSenderId: "1010511734365"
-    };
+    const config = authInfo;
     firebase.initializeApp(config);
   }
   render() {
