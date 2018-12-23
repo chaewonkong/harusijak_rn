8d954d4a57de0180bbe17efae36dfea030f8dc0e
공채원 <chaewonkong@gmail.com>
2018-12-23 15:12:25 +0900

    Map state to props to ListView

--- manager/src/components/EmployeeList.js
+++ manager/src/components/EmployeeList.js
@@ -1,13 +1,32 @@
+import _ from "lodash";
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { ListView, View, Text } from "react-native";
 import { connect } from "react-redux";
 import { employeesFetch } from "../actions";
 
 class EmployeeList extends Component {
   componentWillMount() {
     this.props.employeesFetch();
+
+    this.createDataSource(this.props);
   }
+
+  componentWillReceiveProps(nextProps) {
+    // nextProps are the next set of props
+    //that component will be rendered with.
+    // this.props is still the old set of props
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    this.dataSource = ds.cloneWithRows(employees);
+  }
+
   render() {
+    console.log(this.props);
     return (
       <View>
         <Text>Employee List</Text>
@@ -21,7 +40,16 @@ class EmployeeList extends Component {
   }
 }
 
+const mapStateToProps = props => {
+  // employees wil be an array containing objects
+  const employees = _.map(state.employees, (val, uid) => {
+    return { ...val, uid }; // {shift: "Monday", name: "Leon", id: "jaskdjflan1"}
+  });
+
+  return { employees }; // employees: []
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   { employeesFetch }
 )(EmployeeList);
